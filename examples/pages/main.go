9eab85cfe9a74f0c89e06db4e0596c7a33d5500c
imthaghost <imthaghost@protonmail.com>
2021-05-14 23:25:39 -0700

    pages integration

---
+++ examples/pages/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/tempor1s/notiongo/notion"
+	"log"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Notion Secret Token")
+	flag.Parse()
+}
+
+func main() {
+	// create the notion client to interact with the api
+	instance := notion.NewClient(Token, nil)
+	log.Printf("%+v\n", instance)
+	// retrieve page
+	page, err := instance.Pages.Get(context.Background(), "20c4a81f-4d0f-4b7f-a2d4-27ff6582753d")
+	if err != nil {
+		log.Println("err:", err)
+		return
+	}
+
+	fmt.Printf("Page: %+v \n", page)
+
+}
--- notion/pages.go
+++ notion/pages.go
@@ -1,9 +1,85 @@
 package notion
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type PagesService service
 
+// PropertyValueEnum ...
+type PropertyValueEnum int
+
+const (
+	Test PropertyValueEnum = iota
+
+)
+
+// Page represents the property values of a single Notion page
+type Page struct {
+	Object string `json:"object"`
+	ID string `json:"id"`
+	CreatedTime string `json:"created_time"`
+	LastEditedTime string `json:"last_edited_time"`
+	Archived bool `json:"archived"`
+	// Properties Property `json:"properties"`
+}
+
+// Property ...
+type Property struct {
+	Key string `json:"key"`
+	Value PropertyValue `json:"value"`
+}
+
+// PropertyValue represents the identifier, type, and value of a page property
+type PropertyValue  struct {
+	ID string
+	Type string
+}
+
+
+// DatabaseParent ...
+type DatabaseParent struct {
+	Type string
+	DatabaseID string
+}
+
+// PageParent ...
+type PageParent struct {
+	Type string
+	PageID string
+}
+
+// WorkspaceParent represents a page with a workspace parent is a top-level page within a Notion workspace.
+// The parent property is an object containing the following keys:
+type WorkspaceParent struct {
+	Type string
+}
+
+// Create will
 func (p *PagesService) Create(ctx context.Context) error {
 	return nil
+}
+
+// Get retrieves a Page object using the specified ID
+func (p *PagesService) Get(ctx context.Context, pageID string) (*Page, error) {
+	req, err := p.client.NewRequest("GET", fmt.Sprintf("page/%s", pageID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var page *Page
+	_, err = p.client.Do(ctx, req, &page)
+	if err != nil {
+		return nil, err
+	}
+
+	return page, nil
+}
+
+
+// helpers
+
+func (p PropertyValueEnum) validate() string {
+	return [...]string{"test"}[p]
 }
